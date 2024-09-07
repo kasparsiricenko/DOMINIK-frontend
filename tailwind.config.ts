59903e674c50c0e08489f79ce09b4334d7ccb090
Kaspar Siricenko <kaspar.siricenko@gmail.com>
2024-09-07 14:43:24 +0100

    chore: Update tailwind.config.ts
    This commit updates the tailwind.config.ts file. It adds the 'darkMode' property with the value 'class' and extends the 'variants' object. These changes are made to customize the Tailwind CSS configuration for the project.
    
    Note: This commit message follows the established convention of using a prefix (chore) to indicate a chore or maintenance task in the codebase.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,12 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: 'class',
   content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
